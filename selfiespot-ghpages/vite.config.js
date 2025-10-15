c5f156e354e184277e8eba19ae003a58f503e61c
gardsommerseth-cloud <gardsommerseth@gmail.com>
2025-10-15 21:58:14 +0200

    Delete selfiespot-ghpages directory

--- selfiespot-ghpages/vite.config.js
+++
@@ -1,8 +0,0 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Prototype-for-Selfiespot-app/',
-  plugins: [react()],
-})
